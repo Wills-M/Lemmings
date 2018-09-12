78306a2429b9333282c99334be3144937134a53c
Wills-M <wmm38@drexel.edu>
2018-09-12 15:51:28 -0400

    Game - proof of tech done for lemmings walking along paths and being able to change those at runtime.

---
+++ Game/Lemmings/Assets/Scripts/Lemming.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Lemming : MonoBehaviour {
+
+    public Color color;
+    public Transform goal;
+
+    NavMeshAgent agent;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        if (agent.isOnNavMesh)
+        {
+            NavMeshPath path = new NavMeshPath();
+            agent.CalculatePath(goal.position, path);
+            if (path.status == NavMeshPathStatus.PathComplete)
+            {
+                agent.path = path;
+            }
+        }
+    }
+
+}
---
+++ Game/Lemmings/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerControl : MonoBehaviour {
+
+    public NavMeshSurface surf;
+    public Camera mainCamera;
+    public Material groundRed;
+
+    Ray mouseRay;
+    RaycastHit rayHit;
+    int layerMask;
+    int area;
+
+    private void Start()
+    {
+        //Ignores collisions on all layers except int given
+        layerMask = 1 << 9; //GROUND
+        area = 3; //RED
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            // Raycast from mouse to background layer
+            mouseRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(mouseRay, out rayHit, Mathf.Infinity, layerMask))
+            {
+                GameObject ground = rayHit.collider.gameObject;
+                NavMeshModifier mod = ground.GetComponent<NavMeshModifier>();
+
+                mod.area = area;
+                surf.BuildNavMesh();
+
+                Renderer groundRenderer = ground.GetComponent<Renderer>();
+                groundRenderer.material = groundRed;
+            }
+        }
+    }
+}
